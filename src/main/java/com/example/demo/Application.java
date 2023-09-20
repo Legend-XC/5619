cb0daf752dd3303e4e88be79c8b0748bcd20ca99
Xiangdong Chen <1098936872@qq.com>
2023-09-20 17:16:26 +0800

    application add connection trail (on track)

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@RequestMapping(value = {"/", "/{path:[^\\.]*}", "/{path:^(?!api$).*$}/**"})
+	public String forward() {
+		return "forward:/index.html";}
 }
