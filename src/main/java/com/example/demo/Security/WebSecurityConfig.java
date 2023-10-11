99bd5793b11e99455c7290430d97def41bb46799
Xiangdong Chen <1098936872@qq.com>
2023-10-11 16:45:51 +0800

    web security configuration added, now should d be able to handle login, logout

--- src/main/java/com/example/demo/Security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/Security/WebSecurityConfig.java
@@ -1,9 +1,27 @@
 package com.example.demo.Security;
 
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-@Configuration
-
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(String.valueOf(PathRequest.toStaticResources().atCommonLocations())).permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(formLoginConfigurer -> formLoginConfigurer
+                        .loginPage("/login")
+                        .permitAll())
+                .logout(logoutConfigurer -> logoutConfigurer
+                        .logoutUrl("/logout")
+                        .permitAll());
+
+        return http.build();
+    }
 }
