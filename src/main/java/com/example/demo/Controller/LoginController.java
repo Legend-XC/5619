894021283a88e2253a5c2279b34904d2986eb9d6
Xiangdong Chen <1098936872@qq.com>
2023-10-17 07:05:53 +0800

    securityConfig, LoginController updated. Now can correctly mapping to customized login page

--- src/main/java/com/example/demo/Controller/LoginController.java
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -3,31 +3,34 @@ package com.example.demo.Controller;
 import com.example.demo.Entity.User;
 import com.example.demo.Mapping.UserMapper;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import com.example.demo.Service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Controller
 public class LoginController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/login")
-    public String login(HttpServletRequest request, Model model,
-                        @RequestParam("user_id") int user_id,
-                        @RequestParam("password") String password){
-        User user = userService.authenticate(user_id,password);
-        if(user != null){
-            request.getSession().setAttribute("logged_in_user", user);
-            // by adding to session, the user can be later removed to logout
-            return "redirect:/home"; // to user home page
-        }else{
-            model.addAttribute("error","Invalid user_id or password");
-            return "login"; // back to login page
-        }
-
-    }
+//    @PostMapping("/login")
+//    public String login(HttpServletRequest request, Model model,
+//                        @RequestParam("user_id") int user_id,
+//                        @RequestParam("password") String password){
+//        User user = userService.authenticate(user_id,password);
+//        System.out.println(user);
+//        if(user != null){
+//            request.getSession().setAttribute("logged_in_user", user);
+//            // by adding to session, the user can be later removed to logout
+//            return "redirect:/home"; // to user home page
+//        }else{
+//            model.addAttribute("error","Invalid user_id or password");
+//            return "login"; // back to login page
+//        }
+//
+//    }
 
     @GetMapping("/logout")
     public String logout(HttpServletRequest request){
@@ -39,4 +42,9 @@ public class LoginController {
         return "redirect:/login";
     }
 
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
 }
